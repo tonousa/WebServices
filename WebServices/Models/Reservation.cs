256517f543779431cab262c3906380cbb8bab0bc
tonousa <tonousa@hotmail.com>
2015-08-23 22:11:16 -0400

    666

---
+++ WebServices/Models/Reservation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebServices.Models
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public string ClientName { get; set; }
+        public string Location { get; set; }
+    }
+}
