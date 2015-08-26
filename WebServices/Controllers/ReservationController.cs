cd4d5436f90151d469a3ff1fdc06f95caacc78ad
tonousa <tonousa@hotmail.com>
2015-08-26 08:29:14 -0400

    673

--- WebServices/Controllers/ReservationController.cs
+++ WebServices/Controllers/ReservationController.cs
@@ -4,10 +4,37 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebServices.Models;
 
 namespace WebServices.Controllers
 {
     public class ReservationController : ApiController
     {
+        IReservationRepository repo = ReservationRepository.getRepository();
+
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll();
+        }
+
+        public Reservation GetReservation(int id)
+        {
+            return repo.Get(id);
+        }
+
+        public Reservation PostReservation(Reservation item)
+        {
+            return repo.Add(item);
+        }
+
+        public bool PutReservation(Reservation item)
+        {
+            return repo.Update(item);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            repo.Remove(id);
+        }
     }
 }
